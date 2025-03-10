17f3c2862a59bf2365380522db494dbb2ffb20ec
unknown <shary8748@gmail.com>
2025-03-10 13:01:33 -0700

    modified files

--- script.js
+++ script.js
@@ -36,7 +36,7 @@ document.addEventListener('DOMContentLoaded', () => {
 document.addEventListener('DOMContentLoaded', function () {
     setTimeout(() => {
       document.querySelector('.preloader').style.display = 'none';
-    }, 60000); 
+    }, 6000); 
   });
   
  
